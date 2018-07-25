97c1b4d3835d64b0d50087d038f264587c64a2d5
Patrick Hanson <patrickhanson08@gmail.com>
2018-07-25 14:50:53 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express")
+
+const port = 3000
+
+const url = "mongodb://localhost:27017/rsvp"
+
+const app = express()
+app.use(express.urlencoded())
+app.set("view engine", "pug")
+
+const mongoose = require('mongoose');
+mongoose.connect(url);
+
+const Schema = mongoose.Schema
+const responseSchema = new Schema ({
+    name: String,
+    email: String,
+    attending: Boolean,
+    guestnumber: Number
+})
+
+const Response = mongoose.model('Response', responseSchema)
+
+const rsvp = mongoose.connection;
+
+rsvp.on('error', console.error.bind(console, 'connection error'))
+rsvp.once('open', function() {
+    console.log('we live')
+})
+
+app.listen(port)
+
+app.get('/', function(req, res) {
+    res.render('get')
+})
+
+app.post('/rsvp', function(req, res) {
+    const userResponse = new Response({
+        name: req.body.name,
+        email: req.body.email,
+        attending: req.body.attending,
+        guestnumber: req.body.guestnumber
+    })
+    userResponse.save(function (err, userResponse) {
+        if (err) return console.error(err)
+        res.render('post')
+    })
+})
+
+app.get('/guestlist', function(req, res) {
+    Response.find(function(err, userResponse) {
+        if (err) return console.error(err)
+        console.log(userResponse)
+        res.render('guestlist', ({userResponse}))
+    })
+})
